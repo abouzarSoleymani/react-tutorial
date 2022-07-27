9ee62b8cbb308022399eef2d2290d2798764c6c5
asoleymani <asoleymani@gmail.com>
2022-07-27 21:46:30 +0430

    start

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
 import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import Welcome13 from './function-components/Welcome13'
-import StyleComponent from './style-components/StyleComponent'
-import Second from './state-component/Second'
 
 function App() {
-  const userList: any = [
-    { id: 0, name: 'ali', age: 20 },
-    { id: 1, name: 'reza', age: 30 },
-    { id: 2, name: 'mohammad', age: 35 },
-  ]
+
   return (
     <>
-      <Welcome13 users={userList} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<StyleComponent />} />
-          <Route path="/second">
-            <Route path=":number" element={<Second />} />
-          </Route>
-        </Routes>
-      </BrowserRouter>
     </>
   )
 }
